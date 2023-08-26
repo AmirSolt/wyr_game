e73d673b5a021ffce07056f5a4d3f85b30b19099
AmirSolt <soltamir194@gmail.com>
2023-08-26 06:36:31 -0400

    upd

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ import concurrent.futures
 
 pygame.init()
 
+
+votesA = 1
+votesB = 1
+
+wyrs = [("test", "test")]
+
+# sounds
+
 def game():
     # Set up the drawing window
     screen = pygame.display.set_mode([500, 500])
@@ -25,35 +33,29 @@ def game():
 
     pygame.quit()
 
+
+
+
+
 client: TikTokLiveClient = TikTokLiveClient(unique_id="@ben_toye")
 
 @client.on("connect")
 async def on_connect(_: ConnectEvent):
     print("Connected to Room ID:", client.room_id)
 
-
 @client.on("gift")
 async def on_gift(event: GiftEvent):
     # Streakable gift & streak is over
     if event.gift.streakable and not event.gift.streaking:
         print(f"{event.user.unique_id} sent {event.gift.count}x \"{event.gift.info.name}\"")
-
     # Non-streakable gift
     elif not event.gift.streakable:
         print(f"{event.user.unique_id} sent \"{event.gift.info.name}\"")
 
 
-
-# async def on_comment(event: CommentEvent):
-#     print(f"{event.user.nickname} -> {event.comment}")
-
-# client.add_listener("comment", on_comment)
-
 if __name__ == '__main__':
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        game_future = executor.submit(game)
-        client_future = executor.submit(client.run)
-
-    # You can also handle the results if needed
-    # game_res = game_future.result()
-    # client_res = client_future.result()
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #     game_future = executor.submit(game)
+    #     client_future = executor.submit(client.run)
+        
+    game()
